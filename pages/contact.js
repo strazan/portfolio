2c3b0ffecda4593bdaf4fcf9f1209e787b0a9da7
strazan <sigge@labor.cool>
2020-04-17 14:47:19 +0200

    Revert "Revert "fix: wheel event don't work on mobile -> change to scroll""
    This reverts commit ff67495b2b125f7a12c27354e918659e64f85b72.

--- pages/contact.js
+++ pages/contact.js
@@ -6,13 +6,19 @@ const ContactPage = () => {
   return (
     <Layout>
       <div className={style.contact}>
-        <p className={style.intro}>Hello, here is my contact details. :)</p>
+        {/* <p className={style.intro}>Here is my contact details. :)</p> */}
         <h4 className={style.mail}>sigge@labor.cool</h4>
         <h4 className={style.phone}>+46724494740</h4>
         <div className={style.social}>
-          <a href="">GitHub</a>
-          <a href="">Instagram</a>
-          <a href="">LinkedIn</a>
+          <a target="_blank" href="https://github.com/strazan">
+            GitHub
+          </a>
+          <a target="_blank" href="https://www.instagram.com/s1gge/">
+            Instagram
+          </a>
+          <a target="_blank" href="https://www.linkedin.com/in/siggelabor/">
+            LinkedIn
+          </a>
         </div>
       </div>
     </Layout>
